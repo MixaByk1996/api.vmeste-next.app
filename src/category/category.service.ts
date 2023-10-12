b2b0fbe79480fe7cc2e9ab5c58874393dadef2e1
byk <Mikhail.Byk@visutechsystem.by>
2023-10-12 10:40:59 +0300

    changes

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,13 +1,13 @@
 import {Injectable} from "@nestjs/common";
 import {PrismaService} from "../../prisma/prisma.service";
-import {Category, Prisma} from "@prisma/client";
+import { Prisma} from "@prisma/client";
 
 @Injectable()
 export class CategoryService{
     constructor(private prisma : PrismaService) {
     }
 
-    async getCategoryById(id: string):Promise<Category>{
+    async getCategoryById(id: string){
         return this.prisma.category.findFirst({
             where: {
                 id : Number(id)

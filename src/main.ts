017edeb63447c42f6ebb7d12741e1319db3ed843
Мишка Бык <bykmikhail@gmail.com>
2023-10-14 23:18:47 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -32,14 +32,14 @@ async function bootstrap() {
   https.get(
       `${serverUrl}/swagger/swagger-ui-bundle.js`, function
       (response) {
-        response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+        response.pipe(createWriteStream(process.cwd()+'/swagger-static/swagger-ui-bundle.js'));
         console.log(
             `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
         );
 
       });
   https.get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
-    response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    response.pipe(createWriteStream(process.cwd()+'/swagger-static/swagger-ui-init.js'));
     console.log(
         `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
     );
@@ -49,7 +49,7 @@ async function bootstrap() {
       `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
       function (response) {
         response.pipe(
-            createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+            createWriteStream(process.cwd()+'/swagger-static/swagger-ui-standalone-preset.js'),
         );
         console.log(
             `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
@@ -57,7 +57,7 @@ async function bootstrap() {
       });
 
   https.get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
-    response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    response.pipe(createWriteStream(process.cwd()+'/swagger-static/swagger-ui.css'));
     console.log(
         `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`
     );

342e0897cceaafe1a627ebf6c2577e96b86ebad7
Мишка Бык <bykmikhail@gmail.com>
2023-10-14 22:35:02 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import cookieParser from "cookie-parser";
 import { EventsGateway } from './events/events.gateway';
 import {request} from "express";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { resolve } from 'path';
+import { writeFileSync, createWriteStream } from 'fs';
+import { get } from 'http';
 import * as path from "path";
 import process from "process";
 async function bootstrap() {
@@ -23,16 +26,39 @@ async function bootstrap() {
       .setVersion('1.0')
       .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documentation', app, document,{
-    customJs: [
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js'
-    ],
-    customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-    ],
+  SwaggerModule.setup('swagger', app, document);
+  const serverUrl = "https://api-vmeste-next-app.vercel.app"
+  get(
+      `${serverUrl}/swagger/swagger-ui-bundle.js`, function
+      (response) {
+        response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+        console.log(
+            `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
+        );
+      });
+  get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    console.log(
+        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
+    );
+  });
+
+  get(
+      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+            createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
+        );
+        console.log(
+            `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
+        );
+      });
+
+  get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+    response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    console.log(
+        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
+    );
   });
   await app.listen(3000);
   const eg = app.get(EventsGateway);
